5e977ba6bced45798c18cf1577fbcf7a02af187a
Fathima Rafah <fathimarafah03@gmail.com>
2026-04-08 08:25:16 +0530

    add OTP verification page

---
+++ app/otp-verification/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const OtpVerification = () => {
+    return (
+        <div >
+            <h1>OtpVerification</h1>
+        </div>
+    )
+}
+export default OtpVerification
