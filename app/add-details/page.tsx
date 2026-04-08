6bf637dc86bf24ded3a117d5360b589a53901601
Fathima Rafah <fathimarafah03@gmail.com>
2026-04-08 10:40:39 +0530

    app/add-details/page.tsx

--- app/add-details/page.tsx
+++ app/add-details/page.tsx
@@ -22,24 +22,17 @@ const AddDetails = () => {
                     <div className="bg-white m-[10px] px-[28px] pt-[28px] pb-[28px] rounded-xl  ">
                         <div className="xl:w-[339px] w-[320px] ">
                             <h1 className=" text-color-text text-size-24 font-semibold font-inter leading-[32px] tracking-[0px] pb-[16px]">Add Your Details</h1>
-                            <p className="text-color-text text-size-16 pb-[16px] font-inter font-normal">We’ve sent an SMS to +91 1234 567891</p>
-                            <form className="mt-2">
-                                <div className="relative">
-                                    <label className=" absolute -top-2 left-[14px] bg-white  text-size-13 font-normal text-[#5C5C5C] px-[4px] leading-[16px] tracking-[0px] font-inter z-10">
-                                        SMS Code
-                                    </label>
-                                    <div className="flex items-center gap-2.5 border border-[#D1D5DB] rounded-[14px] p-4 bg-white focus-within:ring-2 focus-within:ring-[#1C3141]/10 focus-within:border-[#1C3141] transition-all">
-                                      
-
-                                        <Input type="number" placeholder="123 456" className="flex-1 bg-transparent border-none  outline-none text-[#1C3141] placeholder:text-[#1C3141]/20 font-inter text-[16px] font-medium leading-[24px] tracking-[0px]" />
+                            <div className="flex flex-col items-center mb-8">
+                                <div className="relative w-[120px] h-[120px] border-2 border-dashed border-[#D1D5DB] rounded-xl flex flex-col items-center justify-center cursor-pointer hover:bg-gray-50 transition-colors">
+                                    <div className="relative">
+                                        <Image src="/icon/file.png" alt="file" width={20} height={20}/>
                                     </div>
+                                    <p className="text-color-placeholder text-[9px] font-inter mt-2 text-center px-2">Add Your Profile picture</p>
                                 </div>
-                            </form>
-                            <p className="pt-[16px] text-color-text text-[11px] text-[#5C5C5C] leading-[16px]">Your 6 digit code is on its way. This can sometimes take a few moments to arrive.</p>
-                            <p className="font-inter text-[14px] font-semibold leading-[24px] tracking-[0px] pt-[16px]  underline underline-offset-0 decoration-solid decoration-[1px] ">Resend code</p>
+                            </div>
                             <Link href="/otp-verification">
                                 <button className="mt-48 font-inter text-size-16 font-semibold leading-[16px] tracking-[0px] text-[#FFFFFF] bg-color-text w-full py-[15px] rounded-[10px] flex items-center justify-center hover:opacity-90 transition-opacity">
-                                Get Started
+                                    Get Started
                                 </button>
                             </Link>
 
