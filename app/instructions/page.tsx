214bb758c5e12aff33dc3c83657ad158a8e2e887
Fathima Rafah <fathimarafah03@gmail.com>
2026-04-08 13:26:01 +0530

    create instruction page

---
+++ app/instructions/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "next/image";
+
+const Instructions=()=>{
+    return(
+        <div className="bg-blue-100">
+            
+           <h1 className="">instructions</h1>
+        </div>
+    )
+}
+export default Instructions
