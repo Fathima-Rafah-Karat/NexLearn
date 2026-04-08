5336ffaae3498963f33a9cdcf2d5acf7af1fa902
Fathima Rafah <fathimarafah03@gmail.com>
2026-04-08 23:09:59 +0530

    improve image styling with padding and rounded corners

--- app/add-details/page.tsx
+++ app/add-details/page.tsx
@@ -113,7 +113,7 @@ const AddDetails = () => {
                                 >
                                     {previewUrl ? (
                                         <>
-                                            <Image src={previewUrl} alt="preview" layout="fill" objectFit="cover" />
+                                            <Image src={previewUrl} alt="preview" layout="fill" objectFit="cover" className="p-1 rounded-xl object-cover" />
                                             <div className="absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center">
                                                 <Camera className="text-white w-6 h-6" />
                                             </div>
