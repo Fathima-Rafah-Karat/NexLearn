7204fa43dac282b54c1af7136dbe26917d99e9ec
Fathima Rafah <fathimarafah03@gmail.com>
2026-04-09 15:39:26 +0530

    remove border from bottom section

--- app/mcq/page.tsx
+++ app/mcq/page.tsx
@@ -297,7 +297,7 @@ const Mcq = () => {
                             ))}
                         </div>
 
-                        <div className="mt-auto pt-8 border-t border-[#E2E8F0]">
+                        <div className="mt-auto pt-8 ">
                             <div className="flex flex-row gap-y-5 gap-x-5">
                                 <div className="flex items-center gap-3 text-[13px] font-medium text-[#1C3141]">
                                     <div className="w-6 h-6 bg-[#4CAF50] rounded-sm" /> Attended
