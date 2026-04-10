e32df322ec7dd61c17cb335c559cd63a5824b971
Fathima Rafah <fathimarafah03@gmail.com>
2026-04-10 15:16:08 +0530

    add environment variable validation with t3-oss/env-nextjs

---
+++ env.ts
@@ -0,0 +1,21 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import * as z from "zod";
+ 
+export const env = createEnv({
+  server: {
+   LOCAL_URL: z.url(),
+   
+  },
+  client: {
+    NEXT_PUBLIC_BASE_URL: z.string().min(1),
+  },
+  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
+  runtimeEnv: {
+    LOCAL_URL: process.env.LOCAL_URL,
+   NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
+  // For Next.js >= 13.4.4, you only need to destructure client variables:
+  // experimental__runtimeEnv: {
+  //   NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+  // }
+});
